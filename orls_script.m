d0c7cefb2dd4a2266ee8ca0e67c7ecc267ac98a8
marija-iloska <50119390+marija-iloska@users.noreply.github.com>
2022-07-05 19:37:27 -0400

    fixed time indices - complete version

--- orls_script.m
+++ orls_script.m
@@ -6,9 +6,9 @@ clc
 var_y = 0.1; % Variance
 g = @(x) x;  % Transition function
 p_s = 0.3;   % Sparsity percent
-dx = 4;      % System dimension
-T = 100;     % Time series length
-r = 0.5;     % Range of input data H
+dx = 7;      % System dimension
+T = 50;     % Time series length
+r = 1;     % Range of input data H
 rt = 5;      % Range of theta
 
 %SSM
@@ -47,15 +47,31 @@ k = 1;
 Hk = H(1:2, k);
 % Update indices in original H
 
+% TIME UPDATE
+% Get theta dimension from last chosen model
+k = length(theta_k);
+
+% Update covariance for new data
+Sigma = var_y*Dk;
+
+% Update gain
+K = Sigma*Hk(2,:)'/(var_y + Hk(2,:)*Sigma*Hk(2,:)');
+
+% Update estimate
+theta_k = theta_k + K*(y(2) - Hk(2,:)*theta_k);
+
+% Update covariance
+Sigma = (eye(k) - K*Hk(2,:))*Sigma;
+
 
 % Start time
-for t = 2 : T-1
+for t = 3 : T-1
 
 
     % ORDER UPDATE
 
     % Compute probability of staying Mk
-    log_pk = log_mvnpdf(y(t), Hk, theta_k, var_y);
+    log_pk = log_mvnpdf(y(1:t), H(1:t, 1:k), theta_k, var_y);
 
 
     % Compute probabilities of moving to Mk+j,
@@ -63,10 +79,10 @@ for t = 2 : T-1
     clear H1_store D1_store theta1_store log_pk1
     for j = 1:(dx - k)
         % Update current theta by jth basis function
-        [theta_temp, D_temp, H_temp] = ols_updates(y(1:t), H, k, j, t, Dk, theta_k);
+        [theta_temp, D_temp, H_temp] = ols_updates(y, H, k, j, t, Dk, theta_k);
 
         % Compute that probability
-        log_pk1(j) = log_mvnpdf(y(t), H_temp, theta_temp, var_y);
+        log_pk1(j) = log_mvnpdf(y(1:t), H_temp, theta_temp, var_y);
 
         % Corresponding variables store
         H1_store{j} = H_temp;
@@ -80,13 +96,13 @@ for t = 2 : T-1
     % Compute probabilities of moving to Mk-j
     % j is an index that iterates through all current basis functions in H
     if (k > 1)
-        clear log_pk0
+        clear log_pk0 H0_store D0_store theta0_store
         for j = 1:k
             % Update current theta by remoivng jth basis function
             [theta_temp, D_temp, H_temp] = ols_downdates(theta_k, Dk, j, H, t);
     
             % Compute that probability
-            log_pk0(j) = log_mvnpdf(y(t), H_temp, theta_temp, var_y);
+            log_pk0(j) = log_mvnpdf(y(1:t), H_temp, theta_temp, var_y);
     
             % Corresponding variables store
             H0_store{j} = H_temp;
@@ -99,7 +115,7 @@ for t = 2 : T-1
     % Normalize Probabilities
     log_probs = [log_pk, log_pk0, log_pk1];
     probs = exp(log_probs - max(log_probs));
-    probs = probs./sum(probs);
+    probs = probs./sum(probs)
 
     k_temp = length(log_pk0);
 
@@ -115,7 +131,7 @@ for t = 2 : T-1
         H = H(:, [setdiff(1:dx, m), m]);
 
         % Update rest
-        Hk = H0_store{m};
+        %Hk = H0_store{m};
         Dk = D0_store{m};
         theta_k = theta0_store{m};
 
@@ -125,18 +141,16 @@ for t = 2 : T-1
         H = H(:, [1:k, m + k - k_temp, setdiff(k+1:dx, m + k - k_temp)]);
 
         % Update rest
-        Hk = H1_store{m - k_temp};
+        %Hk = H1_store{m - k_temp};
         Dk = D1_store{m - k_temp};
         theta_k = theta1_store{m - k_temp};
-
-     else
-         Hk = [Hk; H(t+1, 1:k)];
     end
 
 
     % TIME UPDATE
     % Get theta dimension from last chosen model
     k = length(theta_k);
+    Hk = H(1:t, 1:k);
 
     % Update covariance for new data
     Sigma = var_y*Dk;
