f46cf0806d3b8d7e6bb6abd2bbddbb17005ce91f
Marija Iloska <marija.iloska@stonybrook.edu>
2025-01-10 18:27:00 -0500

    include x axis string input

--- util/bar_plots.m
+++ util/bar_plots.m
@@ -1,7 +1,7 @@
 function [] = bar_plots(features, t0, T, p, K, formats)
 
 
-[fsz, fszl, lwdt, color, grey, c_true, title_str] = formats{:};
+[fsz, fszl, fsz_title, lwdt, color, grey, c_true, title_str, xlabel_str] = formats{:};
 
 pl = bar(t0:T, features', 1.0, 'stacked', 'FaceColor', 'flat', 'FaceAlpha', 1);
 pl(1).CData = color;
@@ -12,9 +12,9 @@ ylim([0, K])
 xlim([t0+1, T])
 set(gca, 'FontSize', 15)
 legend('Correct', 'Incorrect', 'True Dim', 'FontSize', fszl)
-title(title_str, 'FontSize', 20)
+title(title_str, 'FontSize', fsz_title)
 ylabel('Number of Features ', 'FontSize', fsz)
-xlabel('Time', 'FontSize', fsz)
+xlabel(xlabel_str, 'FontSize', fsz)
 box on
 
 
--- util/expectation_plots.m
+++ util/expectation_plots.m
@@ -1,19 +1,20 @@
 function [] = expectation_plots(E, time_plot, D,  formats)
 
 
-[fsz, lwd, col, title_str, y_str] = formats{:};
+[fsz, lwd, col, title_str, y_str, x_str] = formats{:};
 
 
 for j = 1:D
-    plot(time_plot, E(:,j), 'Linewidth',lwd, 'Color', col{j});
+    plot(time_plot, E(:,j), 'Linewidth', lwd, 'Color', col{j});
     hold on
 end
-yline(0, 'k', 'linewidth',1)
+yline(0, 'k', 'linewidth',1.5, 'LineStyle','--')
 set(gca, 'FontSize', fsz)
 title(title_str, 'FontSize', fsz)
 ylabel(y_str, 'FontSize', fsz)
-xlabel('Time', 'FontSize', fsz)
+xlabel(x_str, 'FontSize', fsz)
 xlim([time_plot(1), time_plot(end)])
+box on
 
 
 
