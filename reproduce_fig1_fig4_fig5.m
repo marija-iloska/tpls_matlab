01acc76830a96562eaeb9a7ca4eb9bf599b47040
Marija Iloska <marija.iloska@stonybrook.edu>
2025-01-10 18:47:08 -0500

    reproduce experiment 1 in paper

--- reproduce_fig1_fig4_fig5.m
+++ reproduce_fig1_fig4_fig5.m
@@ -9,16 +9,27 @@ function_paths = [genpath('tpls/'), genpath('util/'), ...
 % Add the paths
 addpath(function_paths)
 
-%% Main Script
+% Main Script ==========================================================
 
 % Settings
-var_y = 0.8;            % Observation noise Variance
+var_y = 1;            % Observation noise Variance
 ps = 5;                 % Number of 0s in theta
 K = 12;                 % Number of available features
 var_features =  1;      % Range of input data H
-var_theta = 0.5;        % Variance of theta
-T = 180;                 % Number of data points
+var_theta = 1;        % Variance of theta
+T = 200;                 % Number of data points
 p = K - ps;             % True model dimension
+
+% SYNTHETIC DATA =================================================================
+%Create data
+[y, H, theta] = generate_data(T, K, var_features, var_theta,  ps, var_y);
+
+% Indices of true features
+idx_h = find(theta ~= 0)';
+
+% Pad original true indices for comparison later
+idx_h_padded = [idx_h zeros(1, K - length(idx_h))];
+
 
 % OLASSO params
 epsilon = 1e-7;
@@ -32,37 +43,30 @@ Ns = 700;
 Nb = 50;
 
 
-% SYNTHETIC DATA =================================================================
-%Create data
-[y, H, theta] = generate_data(T, K, var_features, var_theta,  ps, var_y);
-
-% Indices of true features
-idx_h = find(theta ~= 0)';
-
-% Pad original true indices for comparison later
-idx_h_padded = [idx_h zeros(1, K - length(idx_h))];
-
-
+%% CALL METHODS
 
 
 % TPLS =================================================================
-[theta_tpls, idx_tpls, J, plot_stats] = tpls(y, H, K, var_y, t0, idx_h);
+[theta_tpls, idx_tpls, J, plot_stats, idx_tpls_store] = tpls(y, H, K, var_y, t0, idx_h);
 
 % Results for plotting
 [tpls_correct, tpls_incorrect] = plot_stats{:};
 J_tpls = J;
 
 
-
-
-% Olin LASSO =================================================================
-[theta_olin, idx_olin, J, plot_stats] = olasso(y, H, t0, epsilon, var_y, idx_h);
+% OlinLASSO =================================================================
+[theta_olin, idx_olin, J, plot_stats, idx_olin_store] = olasso(y, H, t0, epsilon, var_y, idx_h);
 
 % Results for plotting
 [olin_correct, olin_incorrect] = plot_stats{:};
 J_olin = J;
 
+% OCCD =================================================================
+[theta_occd, idx_occd, J, plot_stats, idx_occd_store] = occd(y, H, t0, var_y, idx_h);
 
+% Results for plotting
+[occd_correct, occd_incorrect] = plot_stats{:};
+J_occd = J;
 
 
 % RJ MCMC =================================================================
@@ -76,14 +80,13 @@ J_mcmc = J;
 
 
 
-% GROUND TRUTHS ===========================================================
+%% GROUND TRUTHS ===========================================================
 % GENIE
 [J_true, ~] = true_PE(y, H, t0, T, idx_h, var_y);
 
 % SUPER GENIE
 e_super = y(t0+1:end) - H(t0+1:end,:)*theta;
 J_super = cumsum(e_super.^2);
-
 
 
 
@@ -94,6 +97,7 @@ J_super = cumsum(e_super.^2);
 % BARS (for statistical performance)
 tpls_features = [tpls_correct;  tpls_incorrect];
 olin_features = [olin_correct;  olin_incorrect];
+occd_features = [occd_correct;  occd_incorrect];
 mcmc_features = [mcmc_correct;  mcmc_incorrect];
 
 
@@ -104,9 +108,7 @@ mcmc_features = [mcmc_correct;  mcmc_incorrect];
 
 % Colors, FontSizes, Linewidths
 load plot_settings.mat
-fsz = 20;
-fszl= 15;
-
+fszl= 10;
 
 % Time range to plot
 time_plot = t0+1:T;
@@ -117,42 +119,49 @@ figure('Renderer', 'painters', 'Position', [900 100 1000 900])
 
 % TPLS
 subplot(3,2,1)
-formats = {fsz, fszl, lwdt, c_tpls, c_inc, c_true, 'TPLS'};
+formats = {fsz, fszl, fsz_title, lwdt, c_tpls, c_inc, c_true, 'TPLS', 'Time'};
 bar_plots(tpls_features, t0+1, T, p, K, formats)
 
 % OLinLASSO
-subplot(3,2,3)
-formats = {fsz, fszl, lwdt, c_olin, c_inc, c_true, 'OLinLASSO'};
+subplot(3,2,2)
+formats = {fsz, fszl, fsz_title, lwdt, c_olin, c_inc, c_true, 'OLinLASSO', 'Time'};
 bar_plots(olin_features, t0+1, T, p, K, formats)
 
+% OCCD
+subplot(3,2,3)
+formats = {fsz, fszl, fsz_title, lwdt, c_occd, c_inc, c_true, 'OCCD-TWL', 'Time'};
+bar_plots(occd_features, t0+1, T, p, K, formats)
+
 % RJMCMC
-subplot(3,1,3)
-formats = {fsz, fszl, lwdt, c_mcmc, c_inc, c_true, 'RJMCMC'};
+subplot(3,2,4)
+formats = {fsz, fszl, fsz_title, lwdt, c_mcmc, c_inc, c_true, 'RJMCMC', 'Iteration'};
 bar_plots(mcmc_features, 1, Ns, p, K, formats)
 
 
 % PREDICTIVE ERRORs ===============================================
 
 % Difference
-subplot(3,2,4)
+subplot(3,2,6)
 hold on
+plot(time_plot, J_occd - J_true, 'Color', c_occd, 'LineWidth', lwd)
 plot(time_plot, J_olin - J_true, 'Color', c_olin, 'LineWidth', lwd)
 plot(time_plot, J_mcmc - J_true, 'Color', c_mcmc, 'LineWidth', lwd)
 plot(time_plot, J_tpls - J_true, 'Color', c_tpls, 'LineWidth', lwd)
-yline(0, 'Color',c_true, 'linewidth', lwdt)
+yline(0, 'Color',c_true, 'linewidth', 2)
 hold off
 xlim([t0+1, T])
 set(gca, 'FontSize', 15)
-title('Relative', 'FontSize', 20)
-legend('\Delta J_{OLin}', '\Delta J_{RJMCMC}', '\Delta J_{TPLS}', 'FontSize', fszl)
+title('Relative', 'FontSize', fsz_title)
+legend('\Delta J_{OCCD}','\Delta J_{OLin}', '\Delta J_{RJMCMC}', '\Delta J_{TPLS}', 'FontSize', fszl-2)
 xlabel('Time', 'FontSize', fsz)
 ylabel('Predictive Error Difference', 'FontSize', fsz)
 grid on
 box on
 
 % Raw
-subplot(3,2,2)
+subplot(3,2,5)
 hold on
+plot(time_plot, J_occd, 'Color', c_occd, 'LineWidth', lwd)
 plot(time_plot, J_olin,  'Color', c_olin, 'LineWidth', lwd)
 plot(time_plot, J_mcmc,  'Color', c_mcmc, 'LineWidth', lwd)
 plot(time_plot, J_tpls,  'Color', c_tpls, 'LineWidth', lwd)
@@ -161,52 +170,42 @@ plot(time_plot, J_super, 'Color', c_true, 'LineWidth', lwd, 'LineStyle','--')
 hold off
 xlim([t0+1, T])
 set(gca, 'FontSize', 15)
-legend('J_{OLinLASSO}', 'J_{RJMCMC}', 'J_{TPLS}',  'J_{GENIE}', 'J_{TRUTH}',  'FontSize', fszl)
-title('Predictive Error', 'FontSize', 20)
+legend('J_{OCCD}','J_{OLin}', 'J_{RJMCMC}', 'J_{TPLS}',  'J_{GENIE}', 'J_{TRUTH}',  'FontSize', fszl-2)
+title('Predictive Error', 'FontSize', fsz_title)
 ylabel('Predictive Error ', 'FontSize', fsz)
 xlabel('Time', 'FontSize', fsz)
 grid on
 box on
-
-
+ 
+lwd = lwd+0.5;
 
 %% FIGURES 4 and 5:  PLOT EXPECTATIONS
 
 % Import colors
 load colors.mat
+col{7} = [0,0.5,0];
 title_str = 'INSTANT';
 y_str = '\Delta_n';
 
-
+time_plot = time_plot(1:100);
 % ADD A FEATURE =======================================================
-figure('Renderer', 'painters', 'Position', [0 400 450 600])
-
-subplot(2,1,1)
-formats = {fsz, lwd, col, 'INSTANT', '\Delta_n'};
+figure('Renderer', 'painters', 'Position', [0 100 450 900])
+subplot(4,1,1)
+formats = {fsz, lwd, col, 'Extra Feature', '\Delta^{+m}_n', ''};
 expectation_plots(E_add(time_plot,:), time_plot, K-p,  formats)
 
-subplot(2,1,2)
-formats = {fsz, lwd, col, 'BATCH', '\Sigma \Delta_n'};
+subplot(4,1,2)
+formats = {fsz, lwd, col, '', '\Sigma \Delta^{+m}_n', ''};
 expectation_plots(cumsum(E_add(time_plot,:)), time_plot, K-p,  formats)
 
-sgtitle('EXTRA FEATURE:  \Delta_n = E_{+j,n} - E_{p,n}', 'fontsize', fsz)
-
-
 % REMOVING A FEATURE ==================================================
-figure('Renderer', 'painters', 'Position', [450 400 450 600])
-
-col{7} = [0,0,0];
-subplot(2,1,1)
-formats = {fsz, lwd, col, 'INSTANT', '\Delta_n'};
+%col{7} = [0,0,0];
+subplot(4,1,3)
+formats = {fsz, lwd, col, 'Removed Feature', '\Delta^{-m}_n', ''};
 expectation_plots(E_rmv(time_plot,:), time_plot, p,  formats)
 
-subplot(2,1,2)
-formats = {fsz, lwd, col, 'BATCH', '\Sigma \Delta_n'};
+subplot(4,1,4)
+formats = {fsz, lwd, col, '', '\Sigma \Delta^{-m}_n', 'n'};
 expectation_plots(cumsum(E_rmv(time_plot,:)), time_plot, p,  formats)
-
-sgtitle('REMOVED FEATURE: \Delta_n =  E_{-j,n} - E_{p,n}', 'fontsize', 15)
-
-
-
 
 
