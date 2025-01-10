ed889f8f884be1f5f59c048ab4e9f03f988e6567
Marija Iloska <marija.iloska@stonybrook.edu>
2025-01-10 18:47:19 -0500

    reproduce experiments 2 and 3 in paper

--- reproduce_fig2_fig3.m
+++ reproduce_fig2_fig3.m
@@ -12,32 +12,32 @@ clear function_paths
 
 %% Main Script
 
-% FIGURE 3 settings =================================
-K = 15;
-p = 4;
+% FIGURE 2 settings =================================
+% K = 15;
+% p = 4;
 
 % a) observation noise
-% % var_y = 0.01;
+%var_y = 0.01;
 
 % b) 
-var_y = 1;
-
-
-% FIGURE 4 settings =================================
 % var_y = 1;
-% K = 20;
 
-% % a) true model dimension
+
+% FIGURE 3 settings =================================
+var_y = 1;
+K = 20;
+
+% a) true model dimension
 % p =  4;
 
-% % b)
-% p = 14;
+% b)
+p = 14;
 
 
 % Settings
 var_features =  1;       % Range of input data H
 var_theta = 0.5;         % Variance of theta
-T = 1000;                % Number of data points
+T = 100;                % Number of data points
 ps = K - p;              % Number of 0s in theta
 
 % OLASSO params 
@@ -47,20 +47,17 @@ epsilon = 1e-7;
 t0 = K+1;
 
 % Parallel runs
-R = 2;
+R = 4;
 
-tic
 parfor run = 1:R
+    tic
 
     %Create data
     [y, H, theta] = generate_data(T, K, var_features, var_theta,  ps, var_y);
     idx_h = find(theta ~= 0)';
 
-    % Pad original true indices for comparison
-    idx_h_padded = [idx_h zeros(1, K - length(idx_h))];
-
-
-    % JPLS =================================================================
+   
+    % TPLS =================================================================
     [theta_tpls, idx_tpls, J, plot_stats] = tpls(y, H, K, var_y, t0, idx_h);
 
     % Results for plotting
@@ -75,27 +72,39 @@ parfor run = 1:R
     % Results for plotting
     [olin_correct, olin_incorrect] = plot_stats{:};
     J_olin(run,:) = J;
+
+
+
+    % OCCD ===============================================================
+    [theta_occd, idx_occd, J, plot_stats] = occd(y, H, t0, var_y, idx_h);
+    
+    % Results for plotting
+    [occd_correct, occd_incorrect] = plot_stats{:};
+    J_occd(run,:) = J;
     
 
-    % GENIE 
+
+    % GENIE =============================================================
     [J_true(run,:), ~] = true_PE(y, H, t0, T, idx_h, var_y);
 
 
     % BARS (for statistical performance)
     tpls_f(run, :, :) = [tpls_correct;  tpls_incorrect]; 
     olin_f(run, :, :) = [olin_correct;  olin_incorrect]; 
-
-
+    occd_f(run, :, :) = [occd_correct;  occd_incorrect]; 
+    toc
 end
-toc 
+
 
 % Average over R runs - feature plots
 tpls_features = squeeze(mean(tpls_f,1));
 olin_features = squeeze(mean(olin_f,1));
+occd_features = squeeze(mean(occd_f,1));
 
 % Average over R runs - predictive error plots
 J_tpls = mean(J_tpls, 1);
 J_olin = mean(J_olin, 1);
+J_occd = mean(J_occd, 1);
 J_true = mean(J_true,1);
 
 
@@ -103,9 +112,9 @@ J_true = mean(J_true,1);
 
 % Colors, FontSizes, Linewidths
 load plot_settings.mat
+fsz = 16;
+fszl = 12;
 
-fsz = 20;
-fszl = 18;
 
 % Time range to plot
 time_plot = t0+1:T;
@@ -115,30 +124,36 @@ time_plot = t0+1:T;
 % BAR PLOTS SPECIFIC RUN =========================================
 figure('Renderer', 'painters', 'Position', [200 300 1500 400])
 
-% JPLS
-subplot(1,3,1)
-formats = {fsz, fszl, lwdt, c_tpls, c_inc, c_true, 'TPLS'};
+% TPLS
+subplot(1,4,1)
+formats = {fsz, fszl, fsz_title, lwdt, c_tpls, c_inc, c_true, 'TPLS', 'Time'};
 bar_plots(tpls_features, t0+1, T, p, K, formats)
 
 % OLinLASSO
-subplot(1,3,2)
-formats = {fsz, fszl, lwdt, c_olin, c_inc, c_true, 'OLinLASSO'};
+subplot(1,4,2)
+formats = {fsz, fszl, fsz_title, lwdt, c_olin, c_inc, c_true, 'OLinLASSO', 'Time'};
 bar_plots(olin_features, t0+1, T, p, K, formats)
 
+% OCCD
+subplot(1,4,3)
+formats = {fsz, fszl, fsz_title, lwdt, c_occd, c_inc, c_true, 'OCCD-TWL', 'Time'};
+bar_plots(occd_features, t0+1, T, p, K, formats)
+
 % Predictive Error plots
-subplot(1,3,3)
+subplot(1,4,4)
 hold on
+plot(time_plot, J_occd - J_true, 'Color', c_occd, 'LineWidth', lwd)
 plot(time_plot, J_olin - J_true, 'Color', c_olin, 'LineWidth', lwd)
 plot(time_plot, J_tpls - J_true, 'Color', c_tpls, 'LineWidth', lwd)
 yline(0, 'Color',c_true, 'linewidth', lwdt)
 hold off
 xlim([t0+1, T])
 ax = gca;
-box(ax,'on')
+box(ax,'on')    
 ax.BoxStyle ='full';
 ax.FontSize = 15;
 title('Relative', 'FontSize', 20)
-legend('\Delta J_{OLin}', '\Delta J_{TPLS}', 'FontSize', fszl)
+legend('\Delta J_{OCCD}', '\Delta J_{OLin}', '\Delta J_{TPLS}', 'FontSize', fszl)
 xlabel('Time', 'FontSize', fsz)
 ylabel('Predictive Error Difference', 'FontSize', fsz)
 grid on
@@ -147,4 +162,4 @@ grid on
 
 
 
-%save('results/fig4b.mat')
+%save('results/fig3b.mat')
